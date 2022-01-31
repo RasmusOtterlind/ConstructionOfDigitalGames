eb21727758aebb94d2f4ce2a4170c69554b37446
RasOtte <Rasmus.otterlind@gmail.com>
2022-01-31 13:07:48 +0100

    Added simple animations and character

--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -11,10 +11,14 @@ public class PlayerController : MonoBehaviour
     public Transform groundChecker;
     public LayerMask groundMask;
 
+    public Animator animator;
+
+    private Rigidbody rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -29,11 +33,13 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        GetComponent<Rigidbody>().velocity = new Vector3(input * 5, GetComponent<Rigidbody>().velocity.y, 0);
+        rigidbody.velocity = new Vector3(input * 5, GetComponent<Rigidbody>().velocity.y, 0);
+        //Used for animation transitions
+        animator.SetFloat("SpeedX", rigidbody.velocity.x);
         canJump = Physics.CheckSphere(groundChecker.position, 0.1f, groundMask, QueryTriggerInteraction.Ignore);
         if (jump && canJump)
         {
-            GetComponent<Rigidbody>().AddForce(new Vector3(0, 10, 0), ForceMode.Impulse);
+            rigidbody.AddForce(new Vector3(0, 10, 0), ForceMode.Impulse);
             jump = false;
         }
 

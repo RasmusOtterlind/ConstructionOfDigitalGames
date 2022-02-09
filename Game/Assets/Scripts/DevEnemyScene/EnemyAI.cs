18c5060b933724994b3f6f292a47c47286b96bb9
fn890 <fn_890@icloud.com>
2022-02-09 20:19:53 +0100

    Added animation to enemey

--- Game/Assets/Scripts/DevEnemyScene/EnemyAI.cs
+++ Game/Assets/Scripts/DevEnemyScene/EnemyAI.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class EnemyAI : MonoBehaviour
 {
     private Rigidbody rigidbody;
+    public Animator animator;
 
     public LayerMask groundMask;
     public CapsuleCollider capsuleCollider;
     public float desiredDistanceToPlayer = 3f;
+    public float runningSpeed = 3f;
    
     public float detectionRange = 100f;
     public Transform player;
@@ -34,27 +36,37 @@ public class EnemyAI : MonoBehaviour
     private void handleMovement()
     {
         float xDistance = player.transform.position.x - capsuleCollider.transform.position.x;
+        float yDistance = player.transform.position.y - capsuleCollider.transform.position.y;
+        
+        rigidbody.MoveRotation(Quaternion.Euler(new Vector3(0, 90 * Mathf.Sign(player.position.x - capsuleCollider.transform.position.x), 0)));
 
-        if (Mathf.Abs(xDistance) < desiredDistanceToPlayer) return;
+        if (Mathf.Abs(xDistance) < desiredDistanceToPlayer && Mathf.Abs(yDistance) < 1f)
+        {
+            rigidbody.velocity = new Vector3(0, GetComponent<Rigidbody>().velocity.y, 0);
+            animator.SetFloat("SpeedX", 0);
+            return;
+        }
 
         if (xDistance < 0)
         {
             bool leftClear = Physics.CheckSphere(capsuleCollider.transform.position + new Vector3(-capsuleCollider.radius * 2, 0, 0), capsuleCollider.radius, groundMask, QueryTriggerInteraction.Ignore);
             if (leftClear)
             {
-                rigidbody.velocity = new Vector3(-1, GetComponent<Rigidbody>().velocity.x, 0);
+                rigidbody.velocity = new Vector3(-2 * runningSpeed, GetComponent<Rigidbody>().velocity.x, 0);
+                animator.SetFloat("SpeedX", -1 * rigidbody.velocity.x);
             }
 
-        } else
+        }
+        else
         {
             bool rightClear = Physics.CheckSphere(capsuleCollider.transform.position + new Vector3(capsuleCollider.radius * 2, 0, 0), capsuleCollider.radius, groundMask, QueryTriggerInteraction.Ignore);
             if (rightClear)
             {
-                rigidbody.velocity = new Vector3(1, GetComponent<Rigidbody>().velocity.x, 0);
+                rigidbody.velocity = new Vector3(2, GetComponent<Rigidbody>().velocity.x, 0);
+                animator.SetFloat("SpeedX", rigidbody.velocity.x);
             }
         }
     }
-
     private void handleDetection()
     {
         Collider[] colliders = Physics.OverlapSphere(capsuleCollider.transform.position, detectionRange);

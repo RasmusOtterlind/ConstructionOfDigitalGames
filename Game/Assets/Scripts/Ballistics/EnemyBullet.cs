abc92e498214a6577615c5d0284a2d2c30868f58
Bamsefar <bojkat@hotmail.com>
2022-02-11 15:22:52 +0100

    Enemy bullets now get destroyed upon collision with ground or player

---
+++ Game/Assets/Scripts/Ballistics/EnemyBullet.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Player" || collision.collider.gameObject.layer == 6)
+        {
+            Destroy(gameObject);
+        }
+    }
+}

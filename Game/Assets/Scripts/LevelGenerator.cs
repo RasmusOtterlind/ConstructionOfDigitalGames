3373cf7cae67c54e2ca19ef92a6a03418373f323
RasOtte <Rasmus.otterlind@gmail.com>
2022-02-11 16:40:45 +0100

    added dynamic chunks

--- Game/Assets/Scripts/LevelGenerator.cs
+++ Game/Assets/Scripts/LevelGenerator.cs
@@ -9,15 +9,16 @@ public class LevelGenerator : MonoBehaviour
 {
     
     public GameObject[] chunkPrefabs = new GameObject[5];
+    private float nextPosition;
     
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 30; i++)
         {
-            Vector3 spawnPosition = new Vector3(5 + i*12,Random.Range(0, 0), 0f);
+            Vector3 spawnPosition = new Vector3(5 + nextPosition, Random.Range(0, 0), 0f);
             GameObject ground = Instantiate(chunkPrefabs[Random.Range(0, chunkPrefabs.Length)], spawnPosition, Quaternion.identity);
-            
+            nextPosition += ground.GetComponent<ChunkSize>().size;
         }
     }
 

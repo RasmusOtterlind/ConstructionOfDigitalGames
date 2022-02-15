91e55ef1e95a80888df87ceb30be7fb9fc488298
Bamsefar <bojkat@hotmail.com>
2022-02-15 14:31:07 +0100

    Added functionality to UI

--- Game/Assets/Scripts/MainHubScene/UpgradeManager.cs
+++ Game/Assets/Scripts/MainHubScene/UpgradeManager.cs
@@ -7,6 +7,10 @@ using UnityEngine.UI;
 public class UpgradeManager : MonoBehaviour
 {
 
+    private float currentDamage;
+    private float currentHealth;
+    private int currentGold;
+
     [SerializeField] private TextMeshProUGUI goldText;
     [SerializeField] private TextMeshProUGUI damageText;
     [SerializeField] private TextMeshProUGUI healthText;
@@ -14,14 +18,53 @@ public class UpgradeManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        goldText.SetText("Gold: " + PlayerPrefs.GetInt("gold"));
-        damageText.SetText("Damage: " + PlayerPrefs.GetFloat("damage"));
-        healthText.SetText("Health: " + PlayerPrefs.GetFloat("health"));
+        currentDamage = PlayerPrefs.GetFloat("damage");
+        currentHealth = PlayerPrefs.GetFloat("health");
+        currentGold = PlayerPrefs.GetInt("gold");
+        UpdateGoldText();
+        UpdateDamageText();
+        UpdateHealthText();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void UpgradeDamage()
+    {
+        float newDamage = currentDamage += 5;
+        PlayerPrefs.SetFloat("damage", newDamage);
+        UpdateDamageText();
+        UpdateGoldText();
+    }
+
+    public void UpgradeHealth()
+    {
+        float newHealth = currentHealth += 50;
+        PlayerPrefs.SetFloat("health", newHealth);
+        UpdateDamageText();
+        UpdateGoldText();
+    }
+
+    public void CloseWindow()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void UpdateDamageText()
+    {
+        damageText.SetText("Damage: " + currentDamage);
+    }
+
+    private void UpdateHealthText()
+    {
+        healthText.SetText("Health: " + currentHealth);
+    }
+
+    private void UpdateGoldText()
+    {
+        goldText.SetText("Gold: " + currentGold);
     }
 }

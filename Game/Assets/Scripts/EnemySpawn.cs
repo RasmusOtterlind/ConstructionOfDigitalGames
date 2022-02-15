be89f844bb9797f91e7cb9bab2906aa112621d8a
RasmusOtterlind <57478279+RasmusOtterlind@users.noreply.github.com>
2022-02-15 14:27:09 +0100

    added enemy spawner

---
+++ Game/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public GameObject[] enemyPrefabs;
+    public float spawnChance = 0.9f;
+    
+
+    void Start()
+    {
+
+        
+        if(1f - spawnChance > Random.Range(0f, 1f) && transform.parent.position != null)
+        {
+            Instantiate(enemyPrefabs[Random.Range(0, enemyPrefabs.Length)], transform.parent.position + transform.position, Quaternion.identity);
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

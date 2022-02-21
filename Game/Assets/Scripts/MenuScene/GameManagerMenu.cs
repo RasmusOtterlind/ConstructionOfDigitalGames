139fc5479b0087ea1a8bab568dfc6d5466fa1e81
Bamsefar <bojkat@hotmail.com>
2022-02-21 09:53:23 +0100

    Can now change volume of game in main menu

--- Game/Assets/Scripts/MenuScene/GameManagerMenu.cs
+++ Game/Assets/Scripts/MenuScene/GameManagerMenu.cs
@@ -2,12 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
 public class GameManagerMenu : MonoBehaviour
 {
+    private bool audioIsOn = true;
+
+    public Slider volumeSlider;
+
+    private void Start()
+    {
+        PlayerPrefs.GetFloat("volume", 1.0f);
+    }
+
+    private void Awake()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("volume");
+    }
+
     public void Play()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        PlayerPrefs.SetFloat("volume", volumeSlider.value);
     }
 
     public void Exit()

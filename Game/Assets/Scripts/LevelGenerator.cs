ebde4486fc0215015c48c438acb6cbe129e6f185
Bamsefar <bojkat@hotmail.com>
2022-02-21 11:17:13 +0100

    Level generator stuff

--- Game/Assets/Scripts/LevelGenerator.cs
+++ Game/Assets/Scripts/LevelGenerator.cs
@@ -9,17 +9,33 @@ public class LevelGenerator : MonoBehaviour
 {
     
     public GameObject[] chunkPrefabs;
-    private float nextPosition;
+    public GameObject bossChunk;
+    public GameObject endChunk;
+    private float nextPosition = 5f;
+
+    public int levelSize = 15;
     
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 30; i++)
+        for (int i = 0; i < levelSize; i++)
         {
-            Vector3 spawnPosition = new Vector3(5 + nextPosition, Random.Range(0, 0), 0f);
+            Debug.Log(nextPosition);
+            Vector3 spawnPosition = new Vector3(nextPosition, Random.Range(0, 0), 0f);
             GameObject ground = Instantiate(chunkPrefabs[Random.Range(0, chunkPrefabs.Length)], spawnPosition, Quaternion.identity);
             nextPosition += ground.GetComponent<ChunkSize>().size;
         }
+        SpawnFinalChunks();
+    }
+
+    private void SpawnFinalChunks()
+    {
+        Vector3 spawnBossPosition = new Vector3(13.66f + nextPosition, 0, -0.84f);
+        GameObject boss = Instantiate(bossChunk, spawnBossPosition, Quaternion.Euler(0f, 180f, 0f));
+        nextPosition += boss.GetComponent<ChunkSize>().size;
+
+        Vector3 spawnEndPosition = new Vector3(nextPosition, 0, 0f);
+        GameObject end = Instantiate(endChunk, spawnEndPosition, Quaternion.identity);
     }
 
     // Update is called once per frame

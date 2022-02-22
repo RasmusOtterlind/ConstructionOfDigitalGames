e8153dc1df1aa207b3c83cc1dc8f84ff6525bd64
fn890 <fn_890@icloud.com>
2022-02-22 18:47:14 +0100

    Added bombs to boss fight

--- Game/Assets/Scripts/MainHubScene/MainScene/Boss/BossTrigger.cs
+++ Game/Assets/Scripts/MainHubScene/MainScene/Boss/BossTrigger.cs
@@ -9,10 +9,11 @@ public class BossTrigger : MonoBehaviour
     [SerializeField] private GameObject[] fences;
     [SerializeField] private GameObject boss;
 
+    public float bombIntensity = 0.1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -30,12 +31,14 @@ public class BossTrigger : MonoBehaviour
         {
             fence.SetActive(false);
         }
+        GameObject.Find("BombManager").GetComponent<BombManager>().intenisty = 0f;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            GameObject.Find("BombManager").GetComponent<BombManager>().intenisty = bombIntensity;
             Debug.Log("MADE IT");
             foreach (GameObject fence in fences) {
                 fence.SetActive(true);

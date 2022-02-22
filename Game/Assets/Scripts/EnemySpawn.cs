bb01cb8c6b30ee2af5458fed8578ede2ede15202
fn890 <fn_890@icloud.com>
2022-02-22 18:09:44 +0100

    Optimized spawner

--- Game/Assets/Scripts/EnemySpawn.cs
+++ Game/Assets/Scripts/EnemySpawn.cs
@@ -9,16 +9,16 @@ public class EnemySpawn : MonoBehaviour
 
     public GameObject[] enemyPrefabs;
     public float spawnChance = 0.9f;
-    
 
+    private GameObject spawnedEnemy;
+    
     void Start()
     {
-
-        
         if(spawnChance > Random.Range(0f, 1f) && transform.parent.position != null)
         {
             GameObject spawnedPrefab = Instantiate(enemyPrefabs[Random.Range(0, enemyPrefabs.Length)],transform.position, Quaternion.identity);
             spawnedPrefab.transform.position = new Vector3(spawnedPrefab.transform.position.x, spawnedPrefab.transform.position.y, 0f);
+            spawnedEnemy = spawnedPrefab;
         }
 
     }
@@ -26,6 +26,17 @@ public class EnemySpawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+       Camera camera = Camera.main;
+       Vector3 viewPos = camera.WorldToViewportPoint(spawnedEnemy.transform.position);
+        if(Mathf.Abs(viewPos.x) > 3.0f)
+        {
+            spawnedEnemy.SetActive(false);
+        }
+        else
+        {
+            spawnedEnemy.SetActive(true);
+        }
         
     }
 }
